936c59fad185d4dbeefd8c44c16eb7b835bda1db
ranveerkherwa90-blip <ranveerkherwa90@gmail.com>
2026-09-11 12:21:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const http = require('http'); const url = require('url'); const server = http.createServer((req, res) => { const queryObject = url.parse(req.url, true).query; if (req.method === 'GET' && queryObject['hub.verify_token'] === 'My_webhook_token') { res.end(queryObject['hub.challenge']); } else { res.end('Webhook received!'); } }); server.listen(process.env.PORT || 3000, () => { console.log('Server is running'); });
+const http = require('http');
+const url = require('url');
+
+const server = http.createServer((req, res) => {
+    const queryObj = url.parse(req.url, true).query;
+
+    if (req.method === 'GET' && queryObj['hub.verify_token'] === 'my_webhook_token') {
+        res.end(queryObj['hub.challenge']);
+    } else if (req.method === 'POST') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+        req.on('end', () => {
+             console.log('Received data:', body);
+             res.end('EVENT_RECEIVED');
+        });
+    } else {
+        res.end('Webhook received');
+    }
+});
+
+server.listen(process.env.PORT || 3000, () => {
+    console.log('Server is running');
+});
