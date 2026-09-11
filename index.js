6e627903a2949f2cde4fb09f6c46dc033b51f3c4
ranveerkherwa90-blip <ranveerkherwa90@gmail.com>
2026-09-11 12:04:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-const http = require('http');
-const url = require('url');
-const server = http.createServer((req, res) => {
-const queryObject = url.parse(req.url, true).query;
-if (req.method === 'GET' && queryObject['hub.verify_token'] === 'My_webhook_token' && queryObject['hub.challenge']) {
-} else {
-res.end('Webhook received!');
-}
-});
-server.listen(process.env.PORT || 3000, () => {
-console.log('Server is running');
-});
+const http = require('http'); const url = require('url'); const server = http.createServer((req, res) => { const queryObject = url.parse(req.url, true).query; if (req.method === 'GET' && queryObject['hub.verify_token'] === 'My_webhook_token') { res.end(queryObject['hub.challenge']); } else { res.end('Webhook received!'); } }); server.listen(process.env.PORT || 3000, () => { console.log('Server is running'); });
